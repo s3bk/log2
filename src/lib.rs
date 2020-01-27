4a793334ce9ec7e8aa7eaaba7069e14a97cc4564
Sebastian K <s3bk@protonmail.com>
2020-01-27 13:07:53 +0300

    don't do work and throw it away

--- src/lib.rs
+++ src/lib.rs
@@ -28,53 +28,60 @@ pub fn trace(s: &str) {
     console::trace_1(&JsValue::from_str(s));
 }
 pub fn debug(s: &str) {
-    if level() <= Level::Debug {
-        console::log_1(&JsValue::from_str(s));
-    }
+    console::log_1(&JsValue::from_str(s));
 }
 pub fn info(s: &str) {
-    if level() <= Level::Info {
-        console::info_1(&JsValue::from_str(s));
-    }
+    console::info_1(&JsValue::from_str(s));
 }
 pub fn warn(s: &str) {
-    if level() <= Level::Warn {
-        console::warn_1(&JsValue::from_str(s));
-    }
+    console::warn_1(&JsValue::from_str(s));
 }
 pub fn error(s: &str) {
-    if level() <= Level::Debug {
-        console::error_1(&JsValue::from_str(s));
-    }
+    console::error_1(&JsValue::from_str(s));
 }
 
 #[macro_export]
 macro_rules! trace {
-    ($($t:tt)*) => ($crate::trace(&format!($($t)*)))
+    ($($t:tt)*) => (
+        if $crate::tracelevel() <= $crate::Level::Trace {
+            $crate::trace(&format!($($t)*))
+        }
+    )
 }
 
 #[macro_export]
 macro_rules! debug {
-    ($($t:tt)*) => ($crate::debug(&format!($($t)*)))
-}
-
-#[macro_export]
-macro_rules! log {
-    ($($t:tt)*) => ($crate::log(&format!($($t)*)))
+    ($($t:tt)*) => (
+        if $crate::tracelevel() <= $crate::Level::Debug {
+            $crate::debug(&format!($($t)*))
+        }
+    )
 }
 
 #[macro_export]
 macro_rules! info {
-    ($($t:tt)*) => ($crate::info(&format!($($t)*)))
+    ($($t:tt)*) => (
+        if $crate::tracelevel() <= $crate::Level::Info {
+            $crate::info(&format!($($t)*))
+        }
+    )
 }
 
 #[macro_export]
 macro_rules! warn {
-    ($($t:tt)*) => ($crate::warn(&format!($($t)*)))
+    ($($t:tt)*) => (
+        if $crate::tracelevel() <= $crate::Level::Warn {
+            $crate::warn(&format!($($t)*))
+        }
+    )
 }
 
 #[macro_export]
 macro_rules! error {
-    ($($t:tt)*) => ($crate::info(&format!($($t)*)))
+    ($($t:tt)*) => (
+        if $crate::tracelevel() <= $crate::Level::Error {
+            $crate::error(&format!($($t)*))
+        }
+    )
 }
 
